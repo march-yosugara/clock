d36d46febd36b50d7b00530d5175e9441dc4e784
IshikawaMasaaki <ishikawa.masaaki@neut.co.jp>
2020-06-22 19:17:31 +0900

    initialize clock

---
+++ Clock/Clock/frmClock.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Clock
+{
+    partial class Clock
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Clock));
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.LbTime = new System.Windows.Forms.Label();
+            this.LbDate = new System.Windows.Forms.Label();
+            this.NotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.Menu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.MiDisplay = new System.Windows.Forms.ToolStripMenuItem();
+            this.MiExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.Menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // LbTime
+            // 
+            this.LbTime.BackColor = System.Drawing.Color.Transparent;
+            this.LbTime.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.LbTime.Font = new System.Drawing.Font("メイリオ", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(128)), true);
+            this.LbTime.Location = new System.Drawing.Point(0, 41);
+            this.LbTime.Margin = new System.Windows.Forms.Padding(0);
+            this.LbTime.Name = "LbTime";
+            this.LbTime.Size = new System.Drawing.Size(204, 40);
+            this.LbTime.TabIndex = 3;
+            this.LbTime.Text = "00:00:00";
+            this.LbTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LbTime.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Clock_MouseUp);
+            // 
+            // LbDate
+            // 
+            this.LbDate.BackColor = System.Drawing.Color.Transparent;
+            this.LbDate.Dock = System.Windows.Forms.DockStyle.Top;
+            this.LbDate.Font = new System.Drawing.Font("メイリオ", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(128)), true);
+            this.LbDate.Location = new System.Drawing.Point(0, 0);
+            this.LbDate.Margin = new System.Windows.Forms.Padding(0);
+            this.LbDate.Name = "LbDate";
+            this.LbDate.Size = new System.Drawing.Size(204, 40);
+            this.LbDate.TabIndex = 4;
+            this.LbDate.Text = "0000/00/00";
+            this.LbDate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LbDate.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Clock_MouseUp);
+            // 
+            // NotifyIcon
+            // 
+            this.NotifyIcon.ContextMenuStrip = this.Menu;
+            this.NotifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("NotifyIcon.Icon")));
+            this.NotifyIcon.Text = "MarchClock";
+            this.NotifyIcon.Visible = true;
+            // 
+            // Menu
+            // 
+            this.Menu.BackColor = System.Drawing.Color.White;
+            this.Menu.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MiDisplay,
+            this.MiExit});
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(124, 52);
+            // 
+            // MiDisplay
+            // 
+            this.MiDisplay.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.MiDisplay.Name = "MiDisplay";
+            this.MiDisplay.Size = new System.Drawing.Size(180, 24);
+            this.MiDisplay.Text = "Display";
+            this.MiDisplay.Click += new System.EventHandler(this.MiDisplay_Click);
+            // 
+            // MiExit
+            // 
+            this.MiExit.Name = "MiExit";
+            this.MiExit.Size = new System.Drawing.Size(180, 24);
+            this.MiExit.Text = "Exit";
+            this.MiExit.Click += new System.EventHandler(this.MiExit_Click);
+            // 
+            // Clock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(204, 81);
+            this.Controls.Add(this.LbDate);
+            this.Controls.Add(this.LbTime);
+            this.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Clock";
+            this.ShowInTaskbar = false;
+            this.Text = "March Clock";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.Menu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label LbTime;
+        private System.Windows.Forms.Label LbDate;
+        private System.Windows.Forms.NotifyIcon NotifyIcon;
+        private System.Windows.Forms.ContextMenuStrip Menu;
+        private System.Windows.Forms.ToolStripMenuItem MiDisplay;
+        private System.Windows.Forms.ToolStripMenuItem MiExit;
+    }
+}
+
---
+++ Clock/Clock/frmClock.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class Clock : Form
+    {
+        private DateTime dispDateTime = DateTime.Now;
+        private readonly string DateFormat = "yyyy/MM/dd";
+        private readonly string TimeFormat = "HH:mm:ss";
+
+        public Clock()
+        {
+            InitializeComponent();
+
+            this.Disp();
+            this.ControlDispHide("Display");
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (this.dispDateTime.Second != DateTime.Now.Second)
+            {
+                this.dispDateTime = DateTime.Now;
+                this.Disp();
+            }
+        }
+
+        private void Disp()
+        {
+            this.LbDate.Text = this.dispDateTime.ToString(this.DateFormat);
+            this.LbTime.Text = this.dispDateTime.ToString(this.TimeFormat);
+        }
+
+        private void Clock_MouseUp(object sender, MouseEventArgs e)
+        {
+            const int offset_X = 8;
+            const int offset_Y = 31;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                if (this.FormBorderStyle == FormBorderStyle.FixedDialog)
+                {
+                    this.FormBorderStyle = FormBorderStyle.None;
+                    this.Size = new System.Drawing.Size(204, 80);
+                    this.SetDesktopLocation(this.DesktopLocation.X + offset_X, this.DesktopLocation.Y + offset_Y);
+                }
+                else
+                {
+                    this.FormBorderStyle = FormBorderStyle.FixedDialog;
+                    this.Size = new System.Drawing.Size(220, 120);
+                    this.SetDesktopLocation(this.DesktopLocation.X - offset_X, this.DesktopLocation.Y - offset_Y);
+                }
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                this.ControlDispHide("Hide");
+            }
+        }
+
+        private void MiDisplay_Click(object sender, EventArgs e)
+        {
+            this.ControlDispHide(this.MiDisplay.Text);
+        }
+
+        private void MiExit_Click(object sender, EventArgs e)
+        {
+            this.NotifyIcon.Dispose();
+            Application.Exit();
+        }
+
+        private void ControlDispHide(string disp = "Display")
+        {
+            if (disp == "Display")
+            {
+                this.Show();
+                this.MiDisplay.Text = "Hide";
+            }
+            else
+            {
+                this.Hide();
+                this.MiDisplay.Text = "Display";
+            }
+        }
+    }
+}
